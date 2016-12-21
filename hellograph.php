89c80dea97a350a4029ff4653fbdaf9505bf8adc
giansalex <giansalex@gmail.com>
2016-12-21 11:28:01 -0500

    Test GraphQL

--- hellograph.php
+++ hellograph.php
@@ -12,6 +12,70 @@ use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
 use GraphQL\Schema;
 use GraphQL\GraphQL;
+
+class Article{
+    /**
+     * @var int
+     */
+    private $id;
+    /**
+     * @var string
+     */
+    private $name;
+    /**
+     * @var int
+     */
+    private $edad;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getEdad()
+    {
+        return $this->edad;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param int $edad
+     */
+    public function setEdad($edad)
+    {
+        $this->edad = $edad;
+    }
+}
+
 try {
     $queryType = new ObjectType([
         'name' => 'Query',
@@ -20,6 +84,28 @@ try {
                 'type' => Type::string(),
                 'args' => [
                     'message' => ['type' => Type::string()],
+                ],
+                'resolve' => function ($root, $args) {
+                    return $root['prefix'].$args['message'];
+                }
+            ],
+            'version' => [
+                'type' => Type::string(),
+                'resolve' => function(){
+                    return 'v2.3';
+                }
+            ],
+        ],
+    ]);
+
+    $queryType2 = new ObjectType([
+        'name' => 'Query',
+        'fields' => [
+            'echo' => [
+                'type' => Type::listOf(Type::string()),
+                'args' => [
+                    'id' => ['type' => Type::int()],
+                    'nombre' => ['type' => Type::string()],
                 ],
                 'resolve' => function ($root, $args) {
                     return $root['prefix'].$args['message'];
@@ -46,7 +132,9 @@ try {
         'query' => $queryType,
         'mutation' => $mutationType,
     ]);
-    $rawInput = file_get_contents('php://input');
+//    $rawInput = file_get_contents('php://input');
+    if(!isset($_POST['q'])) throw new InvalidArgumentException("Faltan Parametros", -1);
+    $rawInput = $_POST['q'];
     $rootValue = ['prefix' => 'You said: '];
     $result = GraphQL::execute($schema, $rawInput, $rootValue);
 } catch (\Exception $e) {
